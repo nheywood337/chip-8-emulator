675426423e50183070a47703caaddc4609591977
Nate Heywood <github@sl.nateheywood.com>
2026-08-12 10:40:51 -0400

    cleanup main

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,6 @@
 #include <string>
 #include "chip8.h"
 
-const std::string ROM_DIRECTORY = "../roms";
-const std::string ROM_TO_LOAD = ROM_DIRECTORY + "/1-chip8-logo.ch8";
-
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "Invalid execution, use ./chip8 <PATH_TO_ROM>" << std::endl;
@@ -14,7 +11,10 @@ int main(int argc, char* argv[]) {
     const std::string ROM_TO_LOAD = argv[1];
     chip8 chip;
 
-    chip.load_rom(ROM_TO_LOAD);
+    // ROM load failure
+    if (!chip.load_rom(ROM_TO_LOAD)) {
+        return -1;
+    }
 
     return 0;
 }
